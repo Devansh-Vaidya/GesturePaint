2a16b32a08517709faa879ffda512c3be997de0f
Devansh Vaidya <99159017+Devansh-Vaidya@users.noreply.github.com>
2024-05-11 16:07:23 -0400

    Added proper video capture and canvas along with labels for tools

--- main.py
+++ main.py
@@ -1,69 +1,96 @@
 import tkinter as tk
 import cv2
+import mediapipe as mp
+import numpy as np
+import tensorflow as tf
+import time
+import PIL
 from PIL import Image, ImageTk
 
 
-def update_canvas():
-    pass
+class App:
+    def __init__(self, window, window_title, video_source=0):
+        self.photo = None
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
+
+        self.tools = tk.Frame(window)
+        self.tools.grid(row=0, column=1)
+
+        # Labels for brush and eraser
+        self.brush_label = tk.Label(self.tools, text="Brush", bg="black")
+        self.brush_label.grid(row=0, column=0)
+
+        self.eraser_label = tk.Label(self.tools, text="Eraser", bg="black")
+        self.eraser_label.grid(row=0, column=1)
+
+        # Create a canvas that can fit the above video source size
+        self.video_canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.video_canvas.grid(row=1, column=0)
+
+        # Create a drawing canvas
+        self.canvas = tk.Canvas(window, bg="white", cursor="cross", width=self.vid.width, height=self.vid.height)
+        self.canvas.grid(row=1, column=1)
+
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 15
+        self.update()
+
+        self.window.mainloop()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.video_canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
+        self.window.after(self.delay, self.update)
 
 
-def video_capture():
-    _, frame = vid.read()
-    opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    captured_image = Image.fromarray(opencv_image)
-    photo_image = ImageTk.PhotoImage(image=captured_image)
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
 
-    # Displaying photoimage in the label
-    label_widget.photo_image = photo_image
+        # Get video source width and height
+        self.width = 800
+        self.height = 600
 
-    # Configure image in the label
-    label_widget.configure(image=photo_image)
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (None, None)
 
-    # Repeat the same process after every 10 seconds
-    label_widget.after(10, video_capture)
-
-
-def create_canvas():
-    canvas = tk.Canvas(app, bg="white", cursor="cross", width=width, height=height)
-    canvas.grid(row=0, column=1)
-
-
-def init():
-    global vid, width, height, app, label_widget, canvas
-    vid = cv2.VideoCapture(0)
-
-    # Width and height of the video
-    width, height = 80, 60
-
-    # Set the width and height
-    vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    # Get the width and height
-    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # Create a GUI app
-    app = tk.Tk()
-
-    # Bind the app with Escape keyboard to quit app whenever pressed
-    app.bind('<Escape>', lambda e: app.quit())
-
-    # Configure grid to resize proportionally
-    app.grid_rowconfigure(0, weight=1)
-    app.grid_columnconfigure(0, weight=1)
-    app.grid_columnconfigure(1, weight=1)
-
-    # Create a label and display it on app
-    label_widget = tk.Label(app)
-    label_widget.grid(row=0, column=0, sticky="nsew")
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
 
 
 def main():
-    init()
-    create_canvas()
-    video_capture()
-    app.mainloop()
+    # Create a window and pass it to the Application object
+    App(tk.Tk(), "Tkinter and OpenCV")
 
 
 if __name__ == "__main__":
